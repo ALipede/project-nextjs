69fedca4896e2eff2e78d8879700a8fa3bf67998
igi iroko <alipede@igi-iroko.com>
2023-04-13 19:16:25 +0100

    Committing NAVIGATION and LAYOUT

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,7 +1,13 @@
+import { getAllEvents } from '@/dummy-data';
+import EvenList from '@/components/events/event-list';
+
 function AllEventsPage() {
+  const events = getAllEvents();
+
   return (
     <div>
       <h1>All Events</h1>
+      <EvenList items={events} />
     </div>
   );
 }
